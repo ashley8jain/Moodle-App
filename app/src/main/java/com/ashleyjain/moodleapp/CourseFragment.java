4500489a57fdd9fb9dbfa2062819b9da72641f8c
Saurabh Mylavaram <magicalme049@gmail.comgit config --global  user.email magicalme049@gmail.com>
2016-02-20 16:00:55 +0530

    added basic layout structure to backend

--- app/src/main/java/com/ashleyjain/moodleapp/CourseFragment.java
+++ app/src/main/java/com/ashleyjain/moodleapp/CourseFragment.java
@@ -1,18 +1,24 @@
 package com.ashleyjain.moodleapp;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CourseFragment extends Fragment {
-
+public class CourseFragment extends ListFragment implements OnItemClickListener{
 
     public CourseFragment() {
         // Required empty public constructor
@@ -23,7 +29,21 @@ public class CourseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        String coursesJSON = getArguments().getString("courselistJSON");
         return inflater.inflate(R.layout.fragment_course, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.courseMenu, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/ashleyjain/moodleapp/MainActivityFragment.java
+++ app/src/main/java/com/ashleyjain/moodleapp/MainActivityFragment.java
@@ -6,12 +6,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,45 +22,61 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
+    ArrayList<courseObj> courselist = new ArrayList<courseObj>();
 
-    private List createList(){
-        List l = new ArrayList<String>(4);
-        String a = new String("saurabh");
-        String b = new String("chandu");
-        String c = new String("ashley");
-        String d = new String("ricky ponting");
+    private String courselist_JSONstring;
 
-        l.add(a);
-        l.add(b);
-        l.add(c);
-        l.add(d);
-        return l;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        String clist = getArguments().getString("course_list");
+        courselist_JSONstring = clist;
+        try {
+            JSONObject jsonObject = new JSONObject(clist);
+            JSONArray coursearray = jsonObject.getJSONArray("courses");
+            for (int i = 0; i < coursearray.length(); i++) {
+                JSONObject course = coursearray.getJSONObject(i);
+                //System.out.println("<<<<<-----------"+courselist.get(0).getCode()+"&&&"+courselist.get(1).getCode()+"------------------>>>>>>>");
+                courseObj csObj = new courseObj(course.getString("code"), course.getString("name"), course.getString("description"),
+                        course.getString("credits"), course.getString("l_t_p"));
+                courselist.add(i,csObj);
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (getView()==null){
+            View view = inflater.inflate(R.layout.fragment_main, container, false);
+            LinearLayoutManager llManager = new LinearLayoutManager(getActivity());
+            llManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(llManager);
 
-        LinearLayoutManager llManager = new LinearLayoutManager(getActivity());
-        llManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(llManager);
-
-        myRecyleAdapter mAdapter = new myRecyleAdapter(createList());
-        recyclerView.setAdapter(mAdapter);
-        return view;
+            myRecyleAdapter mAdapter = new myRecyleAdapter(courselist);
+            recyclerView.setAdapter(mAdapter);
+            return view;
+        }
+        else{
+            return getView();
+        }
     }
 
     public class myRecyleAdapter extends RecyclerView.Adapter<myRecyleAdapter.myViewHolder>{
 
-        private List<String> stringList;
+        private ArrayList<courseObj> stringList;
+        private String courseJSONstring;
+        private Bundle bundle = new Bundle();
 
-        public myRecyleAdapter(List<String> list){
+        public myRecyleAdapter(ArrayList<courseObj> list){
             this.stringList = list;
+            this.courseJSONstring = courselist_JSONstring;
         }
 
         @Override
@@ -68,28 +86,39 @@ public class MainActivityFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(myViewHolder vHolder,int i){
-            String name = stringList.get(i);
-            vHolder.txtDisplay.setText(name);
+            courseObj Obj = stringList.get(i);
+            //System.out.println("<<<<<-----------the course is "+ Obj.getCode() +"------------------>>>>>>>");
+            vHolder.notxtDisplay.setText(Obj.getCode());
+            vHolder.nameTxtDisplay.setText(Obj.getName());
         }
 
         @Override
         public myViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
             View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_view, viewGroup, false);
-            return new myViewHolder(itemView);
+            myViewHolder vHolder =  new myViewHolder(itemView);
+            vHolder.setI(stringList.get(i).getCode());
+            return vHolder;
         }
 
         public class myViewHolder extends RecyclerView.ViewHolder{
 
-            private TextView txtDisplay;
+            private TextView notxtDisplay;
+            private TextView nameTxtDisplay;
+
+            public void setI(String cCode){
+                bundle.putString("position",cCode);
+            }
 
             public myViewHolder(View view){
                 super(view);
-                txtDisplay = (TextView)view.findViewById(R.id.cardTxtView);
+                notxtDisplay = (TextView)view.findViewById(R.id.courseNo);
+                nameTxtDisplay = (TextView)view.findViewById(R.id.courseName);
                 view.setOnClickListener(new View.OnClickListener(){
 
                     @Override
                     public void onClick(View v) {
                         Fragment courseFrag = new CourseFragment();
+                        courseFrag.setArguments(bundle);
                         FragmentChangeListener frChgListener = (FragmentChangeListener)getActivity();
                         frChgListener.replaceFragment(courseFrag);
                     }
